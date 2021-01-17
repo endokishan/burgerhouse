2532b5d588cb558fc61c6f0331f423462d83ef07
endokishan <endokishan@gmail.com>
2021-01-17 23:17:46 +0530

    added cart page

--- server.ts
+++ server.ts
@@ -9,14 +9,20 @@ const PORT = process.env.PORT || 8000;
 // Assets
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
 // set Template Engine
 app.use(expressLayout);
 app.set('views', path.join(__dirname, '/resources/views'));
 app.set('view engine', 'ejs');
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/cart', (req, res) => {
+    res.render('customers/cart')
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
